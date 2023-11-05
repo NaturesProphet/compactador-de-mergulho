43e7c9d9de0f9eb54b82792bf472b9c963dabd09
Mateus Garcia <mateus.gigainfo@gmail.com>
2023-11-04 23:30:55 -0300

    ajuste

--- script.sh
+++ script.sh
@@ -19,11 +19,11 @@ for video_file in "$input_dir"/*.mp4; do
     output_file="$output_dir/$(basename "$video_file")"
 
     # Comando ffmpeg para comprimir o vídeo (ajuste os parâmetros conforme necessário)
-    # + CRF significa mais compactação e menos qualidade. 32 é razoável.
+    # + CRF significa mais compactação e menos qualidade. 36 é razoável.
     # Atente que quanto mais compactação maior uso de processador e portanto mais demorado é.
     # Essa chamada já utiliza todas as threads disponíveis.
     # Obviamente você precisa do ffmpeg pra rodar o script ( sudo apt-get install ffmpeg )
-    ffmpeg -i "$video_file" -c:v libx264 -crf 32 -c:a aac -strict experimental "$output_file"
+    ffmpeg -i "$video_file" -c:v libx264 -crf 36 -c:a aac -strict experimental -vf "fps=15" -b:v 500K "$output_file"
     
     echo "Vídeo comprimido: $output_file"
   fi
